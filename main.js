de5e43691e0e5414c0d6f8f8bf62cbf29218df26
fredred375 <58181823+fredred375@users.noreply.github.com>
2020-10-06 10:38:40 +0800

    mostly done

--- main.js
+++ main.js
@@ -5,10 +5,35 @@ var button = document.getElementById("cal-button")
 input.value = ''
 color.value = '#b0b0b0'
 
+var selected_cell = cells[0];
+selected_cell.style.backgroundColor = "#000000";
+selected_cell.style.color = "#ffffff";
+for (let index = 0; index < cells.length; index++) {
+    const element = cells[index];
+    element.addEventListener("click", ()=>{
+        selected_cell.style.backgroundColor = "";
+        selected_cell.style.color = "#000000";
+        element.style.backgroundColor = "#000000";
+        element.style.color = "#ffffff";
+        selected_cell = element;
+    })
+}
 
+function inputSchedule()
+{
+    var string = input.value;
+    selected_cell.firstElementChild.innerHTML = string;
+    selected_cell.firstElementChild.style.color = color.value;
+}
 
+button.addEventListener("click", inputSchedule);
 
-
+input.addEventListener("keyup", function(event){
+    if(event.key === 'Enter')
+    {
+        inputSchedule();
+    }
+});
 
 
 
